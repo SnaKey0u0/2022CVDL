59553c41b67bb5eaedebecdc2601ff662718b419
SnaKey0u0 <48859497+SnaKey0u0@users.noreply.github.com>
2022-10-15 23:04:00 +0800

    go home time

--- Hw1/main.py
+++ Hw1/main.py
@@ -5,6 +5,8 @@ from utils import Q0, Q1, Q2, Q3
 import sys
 
 folder_path = None
+imgL_path = None
+imgR_path = None
 
 
 def load_folder():
@@ -12,12 +14,14 @@ def load_folder():
     folder_path = Q0.load_folder()
 
 
-def load_imgL(todo):
-    Q0.load_imgL(todo)
+def load_imgL():
+    global imgL_path
+    imgL_path = Q0.load_img()
 
 
-def load_imgR(todo):
-    Q0.load_imgR(todo)
+def load_imgR():
+    global imgR_path
+    imgR_path = Q0.load_img()
 
 def find_corners():
     if folder_path is not None:
@@ -60,11 +64,25 @@ def show_result():
 
 def show_words_on_board():
     text = ui.txt_2.text()
-    Q2.show_words_on_board(folder_path, text)
+    if len(text)>0 and len(text)<=6:
+        if folder_path is not None:
+            Q2.show_words_on_board(folder_path, text)
+        else:
+            print("You stupid, choose a folder")
+    else:
+        print("You stupid, len of text isn't vaild")
+
+
 
 def show_words_vertically():
     text = ui.txt_2.text()
-    Q2.show_words_on_board(folder_path, text)
+    if len(text)>0 and len(text)<=6:
+        if folder_path is not None:
+            Q2.show_words_on_board(folder_path, text)
+        else:
+            print("You stupid, choose a folder")
+    else:
+        print("You stupid, len of text isn't vaild")
 
 
 def stereo_disparity_map(todo):
@@ -81,8 +99,8 @@ if __name__ == "__main__":
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)  # binding click event
     ui.btn_load_folder.clicked.connect(lambda: load_folder())
-    ui.btn_loadL.clicked.connect(lambda: load_imgL("load_imgL"))
-    ui.btn_loadR.clicked.connect(lambda: load_imgR('load_imgR'))
+    ui.btn_loadL.clicked.connect(lambda: load_imgL())
+    ui.btn_loadR.clicked.connect(lambda: load_imgR())
     ui.btn_corner.clicked.connect(lambda: find_corners())
     ui.btn_intrinsic.clicked.connect(lambda: find_intrinsic())
     ui.btn_extrinsic.clicked.connect(lambda: find_extrinsic())
--- Hw1/utils/Q0.py
+++ Hw1/utils/Q0.py
@@ -3,14 +3,16 @@ import cv2
 
 
 def load_folder():
-    folder_path = QtWidgets.QFileDialog.getExistingDirectory()        # 選取特定資料夾
+    folder_path = QtWidgets.QFileDialog.getExistingDirectory() # 選取特定資料夾
     return folder_path
 
 
-def load_imgL(path):
-    print(path)
+def load_imgL():
+    imgL_path = QtWidgets.QFileDialog.getOpenFileName()
+    print(imgL_path)
 
 
-def load_imgR(path):
-    print(path)
+def load_imgR():
+    imgR_path = QtWidgets.QFileDialog.getOpenFileName()
+    print(imgR_path)
 
