963bd571cb3a21dc58fb0d3b857446b11c3a793e
SnaKey0u0 <48859497+SnaKey0u0@users.noreply.github.com>
2022-10-16 20:50:04 +0800

    fine tune Q3 to hstack

--- Hw1/utils/Q3.py
+++ Hw1/utils/Q3.py
@@ -4,14 +4,14 @@ import numpy as np
 imgR = None
 
 
-def click_event(event, x, y, flags, disparity):
-    eps = 10**-9
-    f = 2826
-    c_rl = 123
-    B = 178
-    if event == cv2.EVENT_LBUTTONDOWN:
-        dot = cv2.circle(imgR, (x-disparity[y][x], y), radius=0, color=(0, 0, 255), thickness=15)
-        cv2.imshow("Right", dot)
+def click_event(event, x, y, flags, args):
+    [disparity, all, img_width] = args
+    try:
+        if event == cv2.EVENT_LBUTTONDOWN:
+            dot = cv2.circle(all, (x-disparity[y][x]+img_width, y), radius=5, color=(0, 0, 255), thickness=-1)
+            cv2.imshow("comb", dot)
+    except Exception as e:
+        pass
 
 
 def stereo_disparity_map(imgL_path, imgR_path):
@@ -26,6 +26,7 @@ def stereo_disparity_map(imgL_path, imgR_path):
                          * 255)  # 正規化到0~1，再乘255、轉回0-255的整數
 
     cv2.namedWindow('Disparity')
+    disparity = cv2.resize(disparity, (0, 0), fx=0.5, fy=0.5)  # 為了呈現方便，可刪
     cv2.imshow("Disparity", disparity)
 
     cv2.waitKey(0)
@@ -35,14 +36,13 @@ def stereo_disparity_map(imgL_path, imgR_path):
 def check_disparity_value(imgL_path, imgR_path):
     global imgR
     imgL = cv2.imread(imgL_path)
-    grayL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
     imgR = cv2.imread(imgR_path)
-    grayR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
 
-    imgL = cv2.resize(imgL, (0, 0), fx=0.5, fy=0.5)
-    grayL = cv2.resize(grayL, (0, 0), fx=0.5, fy=0.5)
-    imgR = cv2.resize(imgR, (0, 0), fx=0.5, fy=0.5)
-    grayR = cv2.resize(grayR, (0, 0), fx=0.5, fy=0.5)
+    imgL = cv2.resize(imgL, (0, 0), fx=0.5, fy=0.5)  # 為了呈現方便，可刪
+    imgR = cv2.resize(imgR, (0, 0), fx=0.5, fy=0.5)  # 為了呈現方便，可刪
+
+    grayL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
+    grayR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
 
     # numDisparities: 最大視差值和最小視差值之差，須為16的倍數
     # blockSize: 匹配的塊大小，須為5~255的奇數
@@ -51,10 +51,8 @@ def check_disparity_value(imgL_path, imgR_path):
     disparity = np.uint8((disparity-np.min(disparity))/(np.max(disparity)-np.min(disparity))
                          * 255)  # 正規化到0~1，再乘255、轉回0-255的整數
 
-    cv2.namedWindow('Left')
-    cv2.setMouseCallback('Left', click_event, disparity)
-    cv2.imshow("Left", imgL)
-    cv2.namedWindow('Right')
-    cv2.imshow("Right", imgR)
+    comb = np.hstack((imgL, imgR))
+    cv2.imshow("comb", comb)
+    cv2.setMouseCallback('comb', click_event, [disparity, comb, imgL.shape[1]])
     cv2.waitKey(0)
     cv2.destroyAllWindows()
