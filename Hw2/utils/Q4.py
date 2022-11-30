14b8344b5d2e01f271ab92ee87a2d62c812800b2
SnaKey0u0 <48859497+SnaKey0u0@users.noreply.github.com>
2022-11-30 08:57:23 +0800

    Q4 finished

--- Hw2/utils/Q4.py
+++ Hw2/utils/Q4.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import copy
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
@@ -11,10 +12,11 @@ def image_reconstruction(folder_path='Dataset_CvDl_Hw2/Q4_Image'):
     global RE
     fig, ax = plt.subplots(6, 10, figsize=(15, 12), subplot_kw={'xticks': [], 'yticks': []})
 
+    original_img = list()
     blues = list()
     greens = list()
     reds = list()
-    
+
     for i, name in enumerate(os.listdir(folder_path)):
         img = cv2.cvtColor(cv2.imread(os.path.join(folder_path, name)), cv2.COLOR_BGR2RGB)
 
@@ -26,32 +28,47 @@ def image_reconstruction(folder_path='Dataset_CvDl_Hw2/Q4_Image'):
         df_green = green/255
         df_red = red/255
 
-        # 保留的特徵數
-        c = 50
-        pca_b = PCA(n_components=c)
-        pca_g = PCA(n_components=c)
-        pca_r = PCA(n_components=c)
+        # print(df_blue.flatten().shape)
+        blues.append(df_blue.flatten())
+        greens.append(df_green.flatten())
+        reds.append(df_red.flatten())
+        original_img.append(copy.deepcopy(img))
 
-        # 降維
-        trans_pca_b = pca_b.fit_transform(df_blue)
-        trans_pca_g = pca_g.fit_transform(df_green)
-        trans_pca_r = pca_r.fit_transform(df_red)
+    c = 25
+    pca_b = PCA(n_components=c)
+    pca_g = PCA(n_components=c)
+    pca_r = PCA(n_components=c)
+    
+    blues = np.array(blues)
+    greens = np.array(greens)
+    reds = np.array(reds)
 
-        # print(trans_pca_b.shape)
-        # print(trans_pca_r.shape)
-        # print(trans_pca_g.shape)
+    # 降維
+    # print(blues.shape)
+    trans_pca_b = pca_b.fit_transform(blues)
+    trans_pca_g = pca_g.fit_transform(greens)
+    trans_pca_r = pca_r.fit_transform(reds)
 
-        # 轉回原維度
-        b_arr = pca_b.inverse_transform(trans_pca_b)
-        g_arr = pca_g.inverse_transform(trans_pca_g)
-        r_arr = pca_r.inverse_transform(trans_pca_r)
+    # 轉回原維度
+    # print(trans_pca_b.shape)
+    b_arr = pca_b.inverse_transform(trans_pca_b)
+    g_arr = pca_g.inverse_transform(trans_pca_g)
+    r_arr = pca_r.inverse_transform(trans_pca_r)
 
-        # 拼回三通道
-        img_reduced = (cv2.merge((b_arr, g_arr, r_arr)))
+    # 拼回三通道
+    # print(b_arr.shape)
+    img_reduced = (cv2.merge((b_arr, g_arr, r_arr))) # BGR => RGB
+
+
+    # 畫圖
+    for i in range(30):
+        # flatten => 350*350*3
+        # print(img_reduced.shape)
+        pca_img = img_reduced[i].reshape(350,350,3)
 
         # gray?
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        gray_img_reduced = cv2.cvtColor((img_reduced*255).astype(np.uint8), cv2.COLOR_BGR2GRAY)
+        gray_img = cv2.cvtColor(original_img[i], cv2.COLOR_BGR2GRAY)
+        gray_img_reduced = cv2.cvtColor((pca_img*255).astype(np.uint8), cv2.COLOR_BGR2GRAY)
 
         # compute reconstruction error
         RE.append(np.sum(np.abs(gray_img-gray_img_reduced)))
@@ -60,9 +77,9 @@ def image_reconstruction(folder_path='Dataset_CvDl_Hw2/Q4_Image'):
         j = i // 10
         k = i % 10
         #plt.title("Original Image")
-        ax[2*j, k].imshow(img)
+        ax[2*j, k].imshow(original_img[i])
         #plt.title("Reduced Image")
-        ax[2*j+1, k].imshow(img_reduced)
+        ax[2*j+1, k].imshow(pca_img)
         ax[2*j, 0].set_ylabel('Original')
         ax[2*j+1, 0].set_ylabel('Reconstructed')
 
@@ -77,3 +94,4 @@ def compute_the_reconstruction_error():
 
 if __name__ == '__main__':
     image_reconstruction()
+    compute_the_reconstruction_error()
